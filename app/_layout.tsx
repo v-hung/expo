1bbe0e2d8899943317ea0efc0caf97472f31675c
Viet Hung <viet.hung.2898@gmail.com>
2023-09-28 17:44:31 +0700

    add

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../components/layouts/Header'
 
 const _layout = () => {
   return (
     <View>
+      <Header />
+      
       <Text>_layout</Text>
     </View>
   )
